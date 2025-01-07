ee82968dc227050687b9181e7133067cec169478
Kekeyydn <149661928+Kekeyydn@users.noreply.github.com>
2025-01-07 11:03:24 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,30 @@
+<div style = "background-color: rgba(0,0,0,0.7);" class="container-fluid py-5 text-light">
+    <div class="container font">
+            <h5 class="text-center mb-4">Temui Kami</h5>
+            <div class="row justify-content-center">
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="http://facebook.com"><i class="fab fa-facebook fs-4 link-light"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="http://instagram.com"><i class="fab fa-instagram fs-4 link-light"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="http://twitter.com"><i class="fab fa-twitter fs-4 link-light"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="http://youtube.com"><i class="fab fa-youtube fs-4 link-light"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="http://whatsapp.com"><i class="fab fa-whatsapp fs-4 link-light"></i></a>
+                </div>
+            </div>
+    </div>
+</div>
+
+
+<div class="container-fluid font py-3 bg-dark text-light">
+    <div class="container d-flex justify-content-between">
+        <label>&copy;2024 BLANCHÉ</label>
+        <label>Owner by 002-009-013</label>
+    </div>
+</div>
---
+++ tentang-kami.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <?php require "navbar.php"; ?>
+
+    <!-- banner -->
+    <div class="container-fluid banner-produk d-flex align-items-center font">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+
+    <!-- main -->
+    <div class="container-fluid py-5 font">
+        <div class="container fs-5 text-center">
+            <p>Selamat datang di Blanchè, tempat di mana kenyamanan dan kesederhanaan bertemu. Didirikan pada 31 Mei 2024, Blanchè hadir untuk menawarkan koleksi pakaian yang elegan dan timeless, dirancang dengan perhatian terhadap detail dan kenyamanan.</p>
+            <p>Di Blanchè, kami percaya bahwa setiap orang berhak tampil memukau dengan pakaian berkualitas tinggi. Kami menggabungkan desain casual dengan sentuhan modern untuk menciptakan pakaian yang tidak hanya indah tetapi juga nyaman dipakai dalam berbagai kesempatan.</p>
+            <p>Dengan komitmen terhadap kualitas dan gaya, kami menghadirkan berbagai pilihan pakaian yang memenuhi kebutuhan fashion Anda. Temukan gaya Anda bersama kami dan rasakan pengalaman berbelanja yang luar biasa di Blanche.Selamat berbelanja!</p>
+        </div>
+    </div>
+
+    <?php require "footer.php"; ?>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+</html>
